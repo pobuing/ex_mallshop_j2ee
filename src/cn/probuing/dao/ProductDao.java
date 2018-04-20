0d06f5ef56abee3616c38c28d87a315ce6a1a8de
王鑫 <330261819@qq.com>
2018-04-20 16:22:51 +0800

    2018.4.19 place company * 2018.4.20 16:22 完成产品列表页

--- src/cn/probuing/dao/ProductDao.java
+++ src/cn/probuing/dao/ProductDao.java
@@ -4,7 +4,9 @@ import cn.probuing.domain.Category;
 import cn.probuing.domain.Product;
 import cn.probuing.utils.DataSourceUtils;
 import org.apache.commons.dbutils.QueryRunner;
+import org.apache.commons.dbutils.handlers.BeanHandler;
 import org.apache.commons.dbutils.handlers.BeanListHandler;
+import org.apache.commons.dbutils.handlers.ScalarHandler;
 
 import java.sql.SQLException;
 import java.util.List;
@@ -46,5 +48,43 @@ public class ProductDao {
         String sql = "select * from category";
         List<Category> categoryList = runner.query(sql, new BeanListHandler<Category>(Category.class));
         return categoryList;
+    }
+
+    /**
+     * 获取商品总条数
+     *
+     * @param cid
+     * @return
+     */
+    public int getCount(String cid) throws SQLException {
+        QueryRunner runner = new QueryRunner(DataSourceUtils.getDataSource());
+        String sql = "select count(*) from product where cid=?";
+        Long query = (Long) runner.query(sql, new ScalarHandler(), cid);
+        return query.intValue();
+    }
+
+    /**
+     * @param cid          分类id
+     * @param index        起始索引
+     * @param currentCount 每页显示条数
+     * @return 商品列表
+     */
+    public List<Product> findProductByPage(String cid, int index, int currentCount) throws SQLException {
+        QueryRunner runner = new QueryRunner(DataSourceUtils.getDataSource());
+        String sql = "select * from product where cid=? limit ?,?";
+        return runner.query(sql, new BeanListHandler<Product>(Product.class), cid, index, currentCount);
+    }
+
+    /**
+     * 通过pid在数据库查询商品
+     *
+     * @param pid
+     * @return
+     */
+    public Product findProductByPid(String pid) throws SQLException {
+        QueryRunner runner = new QueryRunner(DataSourceUtils.getDataSource());
+        String sql = "select * from product where pid= ?";
+        Product product = runner.query(sql, new BeanHandler<Product>(Product.class), pid);
+        return product;
     }
 }
